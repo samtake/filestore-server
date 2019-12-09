048f71355d24d6899bd32b36499726bb11e5a865
huanglongshan689 <943430195@qq.com>
2019-12-10 02:05:26 +0800

    上传接口

--- filestore-server/handler/handler.go
+++ filestore-server/handler/handler.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 //文件上传
@@ -18,5 +20,30 @@ func UploadHandle(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, string(data))
 	} else if r.Method == "POST" {
 		//接收文件流及存储到本地
+		file, head, err := r.FormFile("file")
+		if err != nil {
+			fmt.Printf("Failed to get data err : %s\n", err.Error())
+			return
+		}
+		defer file.Close()
+
+		newFile, err := os.Create("/temp/" + head.Filename)
+		if err != nil {
+			fmt.Printf("failed to create file,err: %s", err.Error())
+			return
+		}
+		defer newFile.Close()
+
+		_, err = io.Copy(newFile, file)
+		if err != nil {
+			fmt.Printf("failed to save file,err: %s", err.Error())
+			return
+		}
+
+		http.Redirect(w, r, "file/upload/suc", http.StatusFound)
 	}
+}
+
+func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Upload Success")
 }
--- filestore-server/main.go
+++ filestore-server/main.go
@@ -9,7 +9,8 @@ import (
 func main() {
 	//N设定路由规则
 	http.HandleFunc("/file/upload", handler.UploadHandle)
-	err := http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)
+	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
 		fmt.Printf("failed to start server,err:%s ", err.Error())
 	}
