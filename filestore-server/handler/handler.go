b87d67f04cae1b361cf1c99118f483e577d7e04e
huanglongshan689 <943430195@qq.com>
2019-12-10 18:49:48 +0800

    文件下载

--- filestore-server/handler/handler.go
+++ filestore-server/handler/handler.go
@@ -75,3 +75,28 @@ func GetFileMetaHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Write(data)
 }
+
+func DownloadHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fsha1 := r.Form.Get("filehash")
+	fm := meta.GetFileMeta(fsha1)
+
+	f, err := os.Open(fm.Location)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+
+	}
+
+	//添加http的响应头，让浏览器可以识别出来
+	w.Header().Set("Content-Type", "application/octect-stream")
+	w.Header().Set("Content-Description", "attachment;filename=\""+fm.FileName+"\"")
+	w.Write(data)
+}
