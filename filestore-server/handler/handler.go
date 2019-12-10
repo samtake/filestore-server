560f69bfd27f88d3c57a1a379c09ce5558aea20d
huanglongshan689 <943430195@qq.com>
2019-12-10 19:44:51 +0800

    删除文件及元信息

--- filestore-server/handler/handler.go
+++ filestore-server/handler/handler.go
@@ -76,6 +76,10 @@ func GetFileMetaHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(data)
 }
 
+func FileQueryHandler() {
+
+}
+
 func DownloadHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	fsha1 := r.Form.Get("filehash")
@@ -99,4 +103,47 @@ func DownloadHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/octect-stream")
 	w.Header().Set("Content-Description", "attachment;filename=\""+fm.FileName+"\"")
 	w.Write(data)
+}
+
+//更新元信息
+func FileMetaUpdateHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	opType := r.Form.Get("op")
+	fileSha1 := r.Form.Get("filehash")
+	newFileName := r.Form.Get("filename")
+
+	if opType != "0" {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	curFileMeta := meta.GetFileMeta(fileSha1)
+	curFileMeta.FileName = newFileName
+	meta.UpdateFileMeta(curFileMeta)
+	data, err := json.Marshal(curFileMeta)
+	if err != nil {
+		w.WriteHeader((http.StatusInternalServerError))
+		return
+	}
+
+	w.WriteHeader((http.StatusOK))
+	w.Write(data)
+}
+
+//删除文件及元信息
+func FileDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fileSha1 := r.Form.Get("filehash")
+
+	fMeta := meta.GetFileMeta(fileSha1)
+	os.Remove(fMeta.Location)
+
+	meta.RemoveFileMeta(fileSha1)
+
+	w.WriteHeader(http.StatusOK)
 }
--- filestore-server/meta/filemeta.go
+++ filestore-server/meta/filemeta.go
@@ -24,3 +24,8 @@ func UpdateFileMeta(fmeta FileMeta) {
 func GetFileMeta(fileSha1 string) FileMeta {
 	return fileMetas[fileSha1]
 }
+
+//删除元信息
+func RemoveFileMeta(fileSha1 string) {
+	delete(fileMetas, fileSha1)
+}
